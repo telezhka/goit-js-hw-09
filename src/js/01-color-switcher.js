09f33a52f00f333dc951b2eacec14ea7f663c052
Roman Telik <rtelik58@gmail.com>
2023-06-11 19:49:17 +0300

    task1-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
-
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const startBtn = document.querySelector('.start');
+const stopBtn = document.querySelector('.stop');
+const body = document.querySelector('body');
+startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', true);
+  timerId = setInterval(() => {
+    let color = getRandomHexColor();
+    body.style.backgroundColor = color;
+    console.log(color);
+  }, 1000);
+});
+stopBtn.addEventListener('click', () => {
+  startBtn.removeAttribute('disabled');
+  clearInterval(timerId);
+});
